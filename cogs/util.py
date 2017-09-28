4023fd868a6baa7d1e9cb5d80a68733a1090f733
shadeyg56 <31134255+shadeyg56@users.noreply.github.com>
2017-09-28 17:15:17 -0500

    Started the addrole command

--- cogs/util.py
+++ cogs/util.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from launcher import utils
 
 class Utils():
     def __init__(self, bot):
@@ -8,6 +9,15 @@ class Utils():
     @commands.command(pass_context = True)
     async def utils(self, ctx):
         await self.bot.say('The util cog is working')
+        
+    @commands.command(pass_context = True)
+    async def addrole(self, ctx, user: discord.Member, role: str):
+        server_roles = [role for role in ctx.message.server.roles if not role.is_everyone]
+        add = discord.utils.get(server_roles, '{}'.format(role))
+        if ctx.message.author.server_permissions.manage_roles:
+            await self.bot.add_roles(user, add)
+            await self.bot.say('I gave {} the {} role'.format(user, role))
+        
         
 def setup(bot):
     bot.add_cog(Utils(bot))
