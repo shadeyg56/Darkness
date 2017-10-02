9b4db73c36a859999b5df6791e7bbc2549314d9f
shadeyg56 <31134255+shadeyg56@users.noreply.github.com>
2017-10-02 16:48:52 -0500

    Update misc.py

--- cogs/misc.py
+++ cogs/misc.py
@@ -13,7 +13,7 @@ class Misc():
 
     def __init__(self, bot):
         self.bot = bot
-        self.ball = ['It is certain', 'It is decidedly so', 'Without a doubt', 'Yes definitely', 'You may rely on it',
+    self.ball = ['It is certain', 'It is decidedly so', 'Without a doubt', 'Yes definitely', 'You may rely on it',
                      'As I see it, yes', 'Most likely', 'Outlook good', 'Yes', 'Signs point to yes',
                      'Reply hazy try again',
                      'Ask again later', 'Better not tell you now', 'Cannot predict now', 'Concentrate and ask again',
