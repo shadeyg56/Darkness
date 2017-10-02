b44dfcee4b38ffdea221789692d756b126a97100
shadeyg56 <31134255+shadeyg56@users.noreply.github.com>
2017-10-02 16:51:19 -0500

    Update misc.py

--- cogs/misc.py
+++ cogs/misc.py
@@ -13,9 +13,8 @@ class Misc():
 
     def __init__(self, bot):
         self.bot = bot
-    self.ball = ['It is certain', 'It is decidedly so', 'Without a doubt', 'Yes definitely', 'You may rely on it',
-                     'As I see it, yes', 'Most likely', 'Outlook good', 'Yes', 'Signs point to yes',
-                     'Reply hazy try again',
+        self.ball = ['It is certain', 'It is decidedly so', 'Without a doubt', 'Yes definitely', 'You may rely on it',
+                     'As I see it, yes', 'Most likely', 'Outlook good', 'Yes', 'Signs point to yes', 'Reply hazy try again',
                      'Ask again later', 'Better not tell you now', 'Cannot predict now', 'Concentrate and ask again',
                      'Don\'t count on it', 'My reply is no', 'My sources say no', 'Outlook not so good',
                      'Very doubtful']
@@ -160,6 +159,7 @@ class Misc():
         embed.set_footer(text='Asked at')
         embed.set_thumbnail(url='http://legomenon.com/images/magic-8ball-first-white.jpg')
         await self.bot.say(embed=embed)
+        await self.bot.delete_messag(ctx.message)
             
     
        
