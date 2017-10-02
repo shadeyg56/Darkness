7feaaa95836ed6a0e6c3812661fa745f9491e1b5
shadeyg56 <31134255+shadeyg56@users.noreply.github.com>
2017-10-01 19:53:31 -0500

    Update util.py

--- cogs/util.py
+++ cogs/util.py
@@ -10,15 +10,15 @@ class Utils():
         await self.bot.say('The util cog is working')
         
     @commands.command(pass_context = True)
-    async def addrole(self, ctx, user: discord.Member, role: str):
+    async def addrole(self, ctx, user: discord.Member, *, role: str):
         server_roles = [role for role in ctx.message.server.roles if not role.is_everyone]
         add = discord.utils.find(lambda m: role.lower() in m.name.lower(), ctx.message.server.roles)
+        if not role:
+            await self.bot.say('That role doesnt exist')
         if ctx.message.author.server_permissions.manage_roles:
             try:
                 await self.bot.add_roles(user, add)
-                await self.bot.say('I gave {} the {} role'.format(user, role))
-                if not role:
-                    await self.bot.say('That role doesnt exist')
+                await self.bot.say('I gave {} the {} role'.format(user, role)
             except discord.Forbidden:
                 await self.bot.say('I need **Manage Roles** for this')
         else:
