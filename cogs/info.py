53deffcb36507e295db446abe3e7127b72f9b8d1
shadeyg56 <31134255+shadeyg56@users.noreply.github.com>
2017-10-04 17:02:13 -0500

    Started serverinfo command

--- cogs/info.py
+++ cogs/info.py
@@ -9,6 +9,20 @@ class Info():
     @commands.command(pass_context=True)
     async def infotest(self, ctx):
         await self.bot.say('The info cog is working')
+      
+    @commands.command(pass_context=True)
+    async def serverinfo(self, ctx):
+        server = ctx.message.server
+        total_members = len(server.members)
+        online = len([x.status for x in server.members
+                      if x.status == discord.Status.online or x.status == discord.Status.idle or x.status == discord.Status.dnd])
+        embed = discord.Embed(title='Server Info', color=0xed)
+        embed.set_author(name=server.name, icon_url=server.icon_url)
+        embed.set_thumbnail(url=server.icon_url)
+        embed.add_field(name='Members', value='{} total\n{} online'.format(members, online))
+                      
+                          
+                       
         
 def setup(bot):
     bot.add_cog(Info(bot))
