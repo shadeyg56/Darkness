1a33f97b155cbf28581866ced54c61501368f8f7
shadeyg56 <31134255+shadeyg56@users.noreply.github.com>
2017-10-05 10:11:21 -0500

    Update serverinfo

--- cogs/info.py
+++ cogs/info.py
@@ -16,10 +16,14 @@ class Info():
         total_members = len(server.members)
         online = len([x.status for x in server.members
                       if x.status == discord.Status.online or x.status == discord.Status.idle or x.status == discord.Status.dnd])
+        date = server.created_at
         embed = discord.Embed(title='Server Info', color=0xed)
         embed.set_author(name=server.name, icon_url=server.icon_url)
         embed.set_thumbnail(url=server.icon_url)
         embed.add_field(name='Members', value='{} total\n{} online'.format(total_members, online))
+        embed.add_field(name='ID', value=server.id)
+        embed.add_field(name="Roles", value=len(server.roles))
+        embed.set_footer(text=date)
         await self.bot.say(embed=embed)
                       
                           
