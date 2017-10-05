2f64ee003d72143e0ced33b5c722b2037c70b287
shadeyg56 <31134255+shadeyg56@users.noreply.github.com>
2017-10-05 18:14:22 -0500

    Serverinfo and userinfo

--- cogs/info.py
+++ cogs/info.py
@@ -17,18 +17,39 @@ class Info():
         online = len([x.status for x in server.members
                       if x.status == discord.Status.online or x.status == discord.Status.idle or x.status == discord.Status.dnd])
         date = server.created_at
+        text_channels = len([m for m in server.channels
+                             if m.type == discord.ChannelType.text])
+        voice_channels = len(server.channels) - text_channels
         embed = discord.Embed(title='Server Info', color=0xed)
         embed.set_author(name=server.name, icon_url=server.icon_url)
         embed.set_thumbnail(url=server.icon_url)
+        embed.add_field(name='Owner', field=server.owner)
+        embed.add_field(name='ID', field=server.id)
         embed.add_field(name='Members', value='{} total\n{} online'.format(total_members, online))
-        embed.add_field(name='ID', value=server.id)
         embed.add_field(name="Roles", value=len(server.roles))
+        embed.add_field(name='Text Channels', value=text_channels)
+        embed.add_field(name='Voice Channels', value=voice_channels)
+        embed.add_field(name='Region', value=server.region)
         embed.set_footer(text=date)
         await self.bot.say(embed=embed)
-                      
+      
+   
+    @commands.command(pass_context=True)
+    async def userinfo(self, ctx, user: discord.Member = None):
+        if user == None:
+            user = ctx.message.author
+        roles = sorted([a for a in user.roles
+                       if x.name != '@everyone'])
+        avatar = user.avatar_url
+        embed = discord.Embed(title='User Info', description='{} is chilling in {} mode'.format(user, user.status), color=0xed)
+        embed.set_author(name=user, icon_url=avatar)
+        embed.set_thumnbail(url=avatar)
+        embed.add_field(name='Nickname', value=user.nick)
+        embed.add_field(name='Roles', value=roles)
                           
                        
         
 def setup(bot):
+   
     bot.add_cog(Info(bot))
   
