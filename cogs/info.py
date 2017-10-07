562403ef07895e8c64471d43a3a223fbeff9b973
shadeyg56 <31134255+shadeyg56@users.noreply.github.com>
2017-10-07 10:59:14 -0500

    Update userinfo

--- cogs/info.py
+++ cogs/info.py
@@ -38,14 +38,19 @@ class Info():
     async def userinfo(self, ctx, user: discord.Member = None):
         if user == None:
             user = ctx.message.author
-        roles = sorted([a for a in user.roles
-                       if a.name != '@everyone'])
+        roles = sorted([a.name a for a in user.roles if a.name != '@everyone'])
         avatar = user.avatar_url
+        created = user.created_at
+        member_number = sorted(server.members,key=lambda m: m.joined_at).index(user) + 1
         embed = discord.Embed(title='User Info', description='{} is chilling in {} mode'.format(user, user.status), color=0xed)
         embed.set_author(name=user, icon_url=avatar)
         embed.set_thumbnail(url=avatar)
+        embed.add_field(name='Account Created', value=created.__format__('%A, %d. %B %Y'))
+        embed.add_field(name='Date Joined', value=user.joined_at.__format__('%A, %d. %B %Y'))
+        embed.add_field(name='Join number', value=member_number)
         embed.add_field(name='Nickname', value=user.nick)
         embed.add_field(name='Roles', value=roles)
+        embed.set_footer(text='ID:' + user.id)
         await self.bot.say(embed=embed)
                          
                        
