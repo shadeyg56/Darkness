c70d7d39cc64ca53c8bcdbc12a061a095b953a59
shadeyg56 <31134255+shadeyg56@users.noreply.github.com>
2017-10-12 17:56:40 -0500

    Update music.py

--- cogs/music.py
+++ cogs/music.py
@@ -1,14 +1,26 @@
 import discord
 from discord.ext import commands
+import youtube_dl
+import opus
 
 class Music():
     
     def __init__(self, bot):
          self.bot = bot
+            
+         self.voice = await self.bot.join_voice_channel
          
     @commands.command()
     async def musictest(self):
         await self.bot.say('The music cog is working')
+        
+    @commands.command(pass_context=True)
+    async def play(self, ctx, songname: str = None):
+        player = await self.voice.create_ytdl_player('https://youtube.com/watch?v=Fz50hqWrHUY')
+        discord.opus.load_opus()
+        player.start()
+        
+        
         
 def setup(bot):
     bot.add_cog(Music(bot))
