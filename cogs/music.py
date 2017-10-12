374d46d0e54424ff89388c187b6c9a1b35e4fb58
shadeyg56 <31134255+shadeyg56@users.noreply.github.com>
2017-10-12 17:29:11 -0500

    Create music.py

---
+++ cogs/music.py
@@ -0,0 +1,14 @@
+import discord
+from discord.ext import commands
+
+class Music():
+    
+    def __init__(self, bot):
+         self.bot = bot
+         
+    @commands.command()
+    async def musictest():
+        await self.bot.say('The music cog is working')
+        
+def setup(bot):
+    bot.add_cog(Music(bot))
