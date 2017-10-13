2dd03fc2ce254ff07432a84e9453211482182ada
shadeyg56 <31134255+shadeyg56@users.noreply.github.com>
2017-10-13 17:12:21 -0500

    Update music.py

--- cogs/music.py
+++ cogs/music.py
@@ -30,7 +30,8 @@ class Music():
     @commands.command(pass_context=True)
     async def disconnect(self, ctx):
         voice = self.bot.join_voice_channel(ctx.message.author.voice.voice_channel)
-        await voice.disconnect
+        state = self.bot.get_voice_state(ctx.message.server)
+        await state.voice.disconnect
     
         
         
