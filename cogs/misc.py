cfef547210810d906bd6b106f2d45f515d44fdb9
shadeyg56 <31134255+shadeyg56@users.noreply.github.com>
2017-10-14 11:57:31 -0500

    Update misc.py

--- cogs/misc.py
+++ cogs/misc.py
@@ -151,29 +151,6 @@ class Misc():
               await self.bot.say('This command can only be ran in the Dragons and Kats server\n. If you arent in this server, you can join with this invite link: https://discord.gg/uEC84cR')
         
         
-    @commands.command(pass_context = True, aliases=['8ball'])
-    async def ball(self, ctx, *, question):
-        ans = random.randint(0, 19)
-        author = ctx.message.author
-        avatar = author.avatar_url
-        timestamp = ctx.message.timestamp
-        embed = discord.Embed(title='8ball', color=0xed, timestamp=timestamp)
-        embed.add_field(name='Question :question:', value='{}'.format(question))
-        embed.add_field(name='Answer :8ball:', value=self.answers[ans])
-        embed.set_footer(text='Asked at')
-        embed.set_thumbnail(url='http://legomenon.com/images/magic-8ball-first-white.jpg')
-        embed.set_author(name=author, icon_url=avatar)
-        await self.bot.say(embed=embed)
-        await self.bot.delete_message(ctx.message)
-        
-    @commands.command(pass_context=True)
-    async def cat(self, ctx):
-        pic = random.randint(0,1)
-        channel = ctx.message.channel
-        x = cat.getCat(directory=None, filename=None, format='{}'.format(self.type[pic]))
-        with open(x, 'rb') as f:
-            await self.bot.send_file(channel, f)
-    
             
     
        
