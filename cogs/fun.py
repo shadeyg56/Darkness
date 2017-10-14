5837323331e6603180856d9b0abba02ff8d0898d
shadeyg56 <31134255+shadeyg56@users.noreply.github.com>
2017-10-14 11:59:58 -0500

    Update fun.py

--- cogs/fun.py
+++ cogs/fun.py
@@ -5,6 +5,12 @@ import cat
 class Fun():
     def __init__(self, bot):
          self.bot = bot
+         self.answers = ['It is certain', 'It is decidedly so', 'Without a doubt', 'Yes definitely', 'You may rely on it',
+                     'As I see it, yes', 'Most likely', 'Outlook good', 'Yes', 'Signs point to yes', 'Reply hazy try again',
+                     'Ask again later', 'Better not tell you now', 'Cannot predict now', 'Concentrate and ask again',
+                     'Don\'t count on it', 'My reply is no', 'My sources say no', 'Outlook not so good',
+                     'Very doubtful']
+        self.type = ['png', 'gif']   
          
     @commands.command()
     async def funtest(self):
