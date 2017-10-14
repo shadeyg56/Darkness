1d38b97ed37662fd5940bf7dcd82003286ec4154
shadeyg56 <31134255+shadeyg56@users.noreply.github.com>
2017-10-14 11:58:17 -0500

    Update fun.py

--- cogs/fun.py
+++ cogs/fun.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+import cat
 
 class Fun():
     def __init__(self, bot):
@@ -8,6 +9,31 @@ class Fun():
     @commands.command()
     async def funtest(self):
         await self.bot.say('The fun cog is working')
+        
+    
+    @commands.command(pass_context = True, aliases=['8ball'])
+    async def ball(self, ctx, *, question):
+        ans = random.randint(0, 19)
+        author = ctx.message.author
+        avatar = author.avatar_url
+        timestamp = ctx.message.timestamp
+        embed = discord.Embed(title='8ball', color=0xed, timestamp=timestamp)
+        embed.add_field(name='Question :question:', value='{}'.format(question))
+        embed.add_field(name='Answer :8ball:', value=self.answers[ans])
+        embed.set_footer(text='Asked at')
+        embed.set_thumbnail(url='http://legomenon.com/images/magic-8ball-first-white.jpg')
+        embed.set_author(name=author, icon_url=avatar)
+        await self.bot.say(embed=embed)
+        await self.bot.delete_message(ctx.message)
+        
+    @commands.command(pass_context=True)
+    async def cat(self, ctx):
+        pic = random.randint(0,1)
+        channel = ctx.message.channel
+        x = cat.getCat(directory=None, filename=None, format='{}'.format(self.type[pic]))
+        with open(x, 'rb') as f:
+            await self.bot.send_file(channel, f)
+    
         
 def setup(bot):
     bot.add_cog(Fun(bot))
