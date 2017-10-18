d2cb4967de885609f030ff064a5bc4d4b1c9c81a
shadeyg56 <31134255+shadeyg56@users.noreply.github.com>
2017-10-17 21:37:29 -0500

    Update fun.py

--- cogs/fun.py
+++ cogs/fun.py
@@ -79,12 +79,11 @@ class Fun():
         
     @commands.command(pass_context=True)
     async def whosthatpokemon(self, ctx):
-        num = random.randint(0, 721)
+        num = random.randint(0, 100)
         pic = 'sprites/sprites/pokemon/{}.png'.format(num)
         with open(pic, 'rb') as f:
             p = PokeAPI()
             x = p.get_pokemon(num)
-            x = self.assertTrue('name' in x)
             embed = discord.Embed(title='Who\'s this Pokemon?', color =0x00FF00)
             await self.bot.send_file(ctx.message.channel, f)
             msg = await self.bot.wait_for_message(timeout=60, author=ctx.message.author)
