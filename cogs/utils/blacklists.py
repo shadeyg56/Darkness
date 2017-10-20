4e56dd95bb7fe8f20afe6e6d3007bd95842a053b
shadeyg56 <31134255+shadeyg56@users.noreply.github.com>
2017-10-19 20:34:14 -0500

    Create blacklists.py

---
+++ cogs/utils/blacklists.py
@@ -0,0 +1,11 @@
+import discord
+class Blacklists():
+   
+    def __init__(self, bot):
+         self.bot = bot
+         
+    def if_blacklisted(self, user):
+        with open('cogs/utils/blacklists.json') as f:
+             blacklist = json.loads(f.read())
+             if blacklist[blacklists][user.id] = user.id:
+                 await self.bot.say('You are blacklisted')
