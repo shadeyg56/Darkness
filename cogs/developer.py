c26a04c10ca7aa6d10b6424c160af6bb982f32da
shadeyg56 <31134255+shadeyg56@users.noreply.github.com>
2018-03-13 17:11:53 -0500

    I hope this doesnt casue any stupid tab errors

--- cogs/developer.py
+++ cogs/developer.py
@@ -1,5 +1,8 @@
 import discord
 from discord.ext import commands
+import subprocess
+import traceback
+
 
 class Developer():
     def __init__(self, bot):
@@ -29,23 +32,106 @@ class Developer():
         await ctx.send("Shutting down...")
         await self.bot.logout()
 
-def cleanup_code( content):
-    """Automatically removes code blocks from the code."""
-    # remove ```py\n```
-    if content.startswith('```') and content.endswith('```'):
-        return '\n'.join(content.split('\n')[1:-1])
-
-    # remove `foo`
-    return content.strip('` \n')        
+    def cleanup_code( content):
+        """Automatically removes code blocks from the code."""
+        # remove ```py\n```
+        if content.startswith('```') and content.endswith('```'):
+            return '\n'.join(content.split('\n')[1:-1])
+        # remove `foo`
+        return content.strip('` \n')        
            
-def get_syntax_error(e):
-    if e.text is None:
-        return '```py\n{0.__class__.__name__}: {0}\n```'.format(e)
-    return '```py\n{0.text}{1:>{0.offset}}\n{2}: {0}```'.format(e, '^', type(e).__name__)
+    def get_syntax_error(e):
+        if e.text is None:
+            return '```py\n{0.__class__.__name__}: {0}\n```'.format(e)
+        return '```py\n{0.text}{1:>{0.offset}}\n{2}: {0}```'.format(e, '^', type(e).__name__)
 
-async def to_code_block(ctx, body):
-    if body.startswith('```') and body.endswith('```'):
-        content = '\n'.join(body.split('\n')[1:-1])
-    else:
-        content = body.strip('`')
-    await bot.edit_message(ctx.message, '```py\n'+content+'```')
+    async def to_code_block(ctx, body):
+        if body.startswith('```') and body.endswith('```'):
+            content = '\n'.join(body.split('\n')[1:-1])
+        else:
+            content = body.strip('`')
+            await self.bot.edit_message(ctx.message, '```py\n'+content+'```')
+    
+    @commands.command()
+    @commands.is_owner()
+    async def terminal(self, ctx, *, command:str):
+        await ctx.send(subprocess.run(command,  cwd='/home/pi/Desktop/Darkness', stdout=subprocess.PIPE, shell=True).stdout.decode('utf-8'))
+        
+    @commands.command()
+    @commands.is_owner()
+    async def update(self, ctx):
+        x = subprocess.run('git pull', cwd='/home/pi/Desktop/Darkness', stdout=subprocess.PIPE, shell=True).stdout.decode('utf-8')
+        try:
+            for module in startup_extensions:
+                self.bot.unload_extension(module)
+                self.bot.load_extension(module)
+                await ctx.send(x)
+                await ctx.send("All Cogs Reloaded")
+                except Exception as e:
+                    await ctx.send(f"Error, Log: \n```py\n{e}```")
+                    
+          
+    @commands.command(name='eval')
+    @commands.is_owner()
+    async def _eval(ctx, *, body: str):
+        '''Run python scripts on discord!'''
+        env = {
+            'bot': bot,
+            'ctx': ctx,
+            'channel': ctx.message.channel,
+            'author': ctx.message.author,
+            'server': ctx.message.guild,
+            'message': ctx.message,
+        }
+        env.update(globals)
+        body = cleanup_code(content=body)
+        stdout = io.StringIO()
+        to_compile = 'async def func():\n%s' % textwrap.indent(body, '  ')
+        try:
+            exec(to_compile, env)
+        except SyntaxError as e:
+            return await ctx.send(get_syntax_error(e))
+        func = env['func']
+        try:
+            with redirect_stdout(stdout):
+                ret = await func()
+        except Exception as e:
+            value = stdout.getvalue()
+            x = await ctx.send('```py\n{}{}\n```'.format(value, traceback.format_exc()))
+            try:
+                await bot.add_reaction(x, '\U0001f534')
+            except:
+                pass
+        else:
+            value = stdout.getvalue()
+        
+        if TOKEN in value:
+            value = value.replace(TOKEN,"[EXPUNGED]")
+            
+        if ret is None:
+            if value:
+                try:
+                    x = await ctx.send('```py\n%s\n```' % value)
+                except:
+                    x = await ctx.send('```py\n\'Result was too long.\'```')
+                try:
+                    await bot.add_reaction(x, '\U0001f535')
+                except:
+                    pass
+            else:
+                try:
+                    await bot.add_reaction(ctx.message, '\U0001f535')
+                except:
+                    pass
+        else:
+            try:
+                x = await ctx.send('```py\n%s%s\n```' % (value, ret))
+            except:
+                x = await ctx.send('```py\n\'Result was too long.\'```')
+            try:
+                await bot.add_reaction(x, '\U0001f535')
+            except:
+                pass
+            
+def setup(bot):
+    bot.add_cog(Developer(bot))
