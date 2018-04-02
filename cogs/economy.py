84af4ac2ccbd45d9b7430cc328a7eb8f2648fb8c
shadeyg56 <shadeyg56@gmail.com>
2018-04-02 14:50:59 -0500

    maybe this will save stuff

--- cogs/economy.py
+++ cogs/economy.py
@@ -5,28 +5,31 @@ import json
 class Economy():
 	def __init__(self, bot):
 		self.bot = bot
-		with open('cogs/utils/economy.json') as f:
-			self.data = json.load(f)
+		
 
 	@commands.command()
 	async def openaccount(self, ctx):
-		if str(ctx.author.id) not in self.data:
+		with open('cogs/utils/economy.json') as f:
+			data = json.load(f)
+		if str(ctx.author.id) not in data:
 			await ctx.send("Thanks for joining the National Darkness Bank. As a welcome gift we have given you 200 Darkness chips <:Darkness:411673568170999808>")
-			self.data[str(ctx.author.id)] = {}
-			self.data[str(ctx.author.id)]["name"] = ctx.author
-			self.data[str(ctx.author.id)]["servers"] = {}
-			self.data[str(ctx.author.id)]["servers"][ctx.guild] = ctx.guild.id
-			self.data[str(ctx.author.id)]["balance"] = 200
+			data[str(ctx.author.id)] = {}
+			data[str(ctx.author.id)]["name"] = str(ctx.author)
+			data[str(ctx.author.id)]["servers"] = {}
+			data[str(ctx.author.id)]["servers"][ctx.guild] = ctx.guild.id
+			data[str(ctx.author.id)]["balance"] = 200
 		else:
 			await ctx.send("You've already opened an account silly")
-		data = json.dumps(self.data, indent=4)
+		data = json.dumps(data, indent=4)
 		with open('cogs/utils/economy.json', 'w') as f:
 			f.write(data)
 
 	@commands.command()
 	async def balance(self, ctx):
-		if str(ctx.author.id) in self.data:
-			embed = discord.Embed(title="Balance", description=f"{self.data[str(ctx.author.id)]['balance']} Darkness Chips <:Darkness:411673568170999808>", color=discord.Color.blue())
+		with open('cogs/utils/economy.json') as f:
+			data = json.load(f)
+		if str(ctx.author.id) in data:
+			embed = discord.Embed(title="Balance", description=f"{data[str(ctx.author.id)]['balance']} Darkness Chips <:Darkness:411673568170999808>", color=discord.Color.blue())
 			await ctx.send(embed=embed)
 		else:
 			await ctx.send("You haven't opened an account yet. To do so run the openaccount command.")
