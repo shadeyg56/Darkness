0ba7163c3a7ac54a596d16f07a2f25b66d89a8fa
shadeyg56 <shadeyg56@gmail.com>
2018-04-02 14:07:50 -0500

    syntax error

--- cogs/economy.py
+++ cogs/economy.py
@@ -20,7 +20,7 @@ class Economy():
 			await ctx.send("You've already opened an account silly")
 
 	@commands.command()
-	async def balance()
+	async def balance():
 		if str(ctx.author.id) in self.data:
 			embed = discord.Embed(title="Balance", description=f"{self.data[str(ctx.author.id)]["balance"]} Darkness Chips <:Darkness:411673568170999808>", color=discord.Color.blue())
 			await ctx.send(embed=embed)
